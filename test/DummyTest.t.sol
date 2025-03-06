abc71e4a70f932eb0ba0102529a39dd433e9bdab
Zo <zo@lilsilmac.myfiosgateway.com>
2025-03-06 01:00:32 -0500

    Added dummy test to fix CI error

---
+++ test/DummyTest.t.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {Test} from "forge-std/Test.sol";
+
+contract DummyTest is Test {
+    function testDummy() public {
+        assertTrue(true);
+    }
+}
